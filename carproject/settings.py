9bfc22a4cb140856f983ce315fd248406994b124
WU Yue <yuewuxd@gmail.com>
2025-01-06 11:19:27 +0800

    update gitignore and requirements.txt

--- carproject/settings.py
+++ carproject/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "django_filters",
+    "hypothesis",
     "rest_framework",
     "debug_toolbar",
     "cars",

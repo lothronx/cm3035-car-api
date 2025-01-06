04321c17d3305de23da682bfef9246dd193a6e40
WU Yue <yuewuxd@gmail.com>
2025-01-06 19:04:48 +0800

    home page

--- cars/views.py
+++ cars/views.py
@@ -1,7 +1,6 @@
 from rest_framework.views import APIView
-from rest_framework.response import Response
-
+from django.shortcuts import render
 
 class IndexView(APIView):
     def get(self, request):
-        return Response("ok")
+        return render(request, 'index.html')
